c6bd09268297c1a4bc4816056dafad37463772b8
Under <cabmanbellou@hotmail.com>
2021-12-02 13:52:20 -0600

    Add 1.7.10 files

--- src/main/java/com/theundertaker11/staffoftravelingkeybind/KeybindHandler.java
+++ src/main/java/com/theundertaker11/staffoftravelingkeybind/KeybindHandler.java
@@ -2,34 +2,55 @@ package com.theundertaker11.staffoftravelingkeybind;
 
 import org.lwjgl.input.Keyboard;
 
-import com.theundertaker11.staffoftravelingkeybind.packets.PacketHandler;
-import com.theundertaker11.staffoftravelingkeybind.packets.TeleportPacket;
-
+import cpw.mods.fml.client.registry.ClientRegistry;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.TickEvent.PlayerTickEvent;
+import cpw.mods.fml.relauncher.Side;
+import crazypants.enderio.EnderIO;
+import crazypants.enderio.teleport.TravelController;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
-import net.minecraftforge.fml.client.registry.ClientRegistry;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 
-@EventBusSubscriber
 public class KeybindHandler {
-	public static KeyBinding keybindStaffTeleport;
+    public static KeyBinding keybindStaffTeleport;
 
-	public static void init() {
+    public static void init() {
+        keybindStaffTeleport = new KeyBinding("Staff of Travel Teleport", Keyboard.KEY_G, "Staff Of Traveling Keybind");
+        ClientRegistry.registerKeyBinding(keybindStaffTeleport);
+    }
 
-		keybindStaffTeleport = new KeyBinding("Staff of Travel Teleport", Keyboard.KEY_G, "Staff Of Traveling Keybind");
-		ClientRegistry.registerKeyBinding(keybindStaffTeleport);
-	}
+    private static long lastBlickTick = -1;
 
-	@SideOnly(Side.CLIENT)
-	@SubscribeEvent
-	public static void clientPlayerTick(TickEvent.PlayerTickEvent event) {
 
-		if (KeybindHandler.keybindStaffTeleport.isPressed()) {
-			PacketHandler.INSTANCE.sendToServer(new TeleportPacket());
-		}
+    @SubscribeEvent
+    public void clientPlayerTick(PlayerTickEvent event) {
+        if(event.side == Side.CLIENT){
+            if (KeybindHandler.keybindStaffTeleport.isPressed()) {
+                // PacketHandler.INSTANCE.sendToServer(new TeleportPacket());
+                EntityPlayer player = Minecraft.getMinecraft().thePlayer;
+                ItemStack equipped = null;
+                for (int i = 0; i < player.inventory.getSizeInventory(); i++) {
+                    ItemStack stack = player.inventory.getStackInSlot(i);
+                    if (stack != null) {
+                        if(stack.getItem() == EnderIO.itemTravelStaff) {
+                            equipped = stack;
+                            break;
+                        }
+                    }
+                }
+                long ticksSinceBlink = EnderIO.proxy.getTickCount() - lastBlickTick;
+                if (ticksSinceBlink < 0) {
+                    lastBlickTick = -1;
+                }
+                if (ticksSinceBlink >= 10) {
+                    if (equipped != null && TravelController.instance.doBlink(equipped, player)) {
+                        lastBlickTick = EnderIO.proxy.getTickCount();
+                    }
+                }
+            }
+        }
 
-	}
+    }
 }
--- src/main/java/com/theundertaker11/staffoftravelingkeybind/SOTKeybindMain.java
+++ src/main/java/com/theundertaker11/staffoftravelingkeybind/SOTKeybindMain.java
@@ -1,34 +1,34 @@
 package com.theundertaker11.staffoftravelingkeybind;
 
-import net.minecraft.init.Blocks;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.relauncher.Side;
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.relauncher.Side;
+import net.minecraftforge.common.MinecraftForge;
 
-import org.apache.logging.log4j.Logger;
-
-import com.theundertaker11.staffoftravelingkeybind.packets.PacketHandler;
-
-@Mod(modid = SOTKeybindMain.MODID, name = SOTKeybindMain.NAME, version = SOTKeybindMain.VERSION)
+@Mod(modid = SOTKeybindMain.MODID, name = SOTKeybindMain.NAME, version = SOTKeybindMain.VERSION, acceptedMinecraftVersions = "*")
 public class SOTKeybindMain
 {
-    public static final String MODID = "staffoftravelingkeybind";
+    public static final String MODID = "sotkeybind";
     public static final String NAME = "Staff Of Traveling Keybind";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.3";
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
-    	PacketHandler.init();
-    	if (event.getSide() == Side.CLIENT)
-			KeybindHandler.init();
+        if (event.getSide() == Side.CLIENT) {
+            KeybindHandler.init();
+        }
     }
 
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-    	
+        if (event.getSide() == Side.CLIENT) {
+            MinecraftForge.EVENT_BUS.register(new KeybindHandler());
+            FMLCommonHandler.instance().bus().register(new KeybindHandler());
+        }
     }
 }
